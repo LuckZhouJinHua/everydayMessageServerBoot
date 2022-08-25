9500100d34d0eb12d7c014a35f1be3692c5258c8
cole.zhou <1253980201@qq.com>
2022-08-25 17:59:07 +0800

    添加定时任务

--- src/main/java/com/zjh/everydaymessageserverboot/dingdong/task/SchedulerTask.java
+++ src/main/java/com/zjh/everydaymessageserverboot/dingdong/task/SchedulerTask.java
@@ -24,25 +24,57 @@ public class SchedulerTask {
     private static final Logger logger = LoggerFactory.getLogger(SchedulerTask.class);
 
     /**
-     * @Description:  每天早上8:30执行一次
-     * @Param:  []
-     * @return:  void
-     * @Author:  cole.zhou
-     * @Date:  2022/8/24 下午 1:39
+     * @Description: 每天早上7:30执行一次
+     * @Param: []
+     * @return: void
+     * @Author: cole.zhou
+     * @Date: 2022/8/24 下午 1:39
      */
     @Scheduled(cron = "0 * * * * ?")
-    public void scheduled1() {
-
+    public void morningTask() {
         WeatherInfo weather = GaodeUtil.getNowWeatherInfo(getAdcCode("湖北省", "武汉市"));
-       logger.info("=====>>>>>使用cron1执行定时任务");
-       logger.info("返回数据{}" , JSONUtil.toJsonStr(weather));
+        logger.info("=====>>>>>使用cron1执行定时任务");
+        logger.info("返回数据{}", JSONUtil.toJsonStr(weather));
     }
 
+    /**
+     * @Description: 每天中午12:00执行一次
+     * @Param: []
+     * @return: void
+     * @Author: cole.zhou
+     * @Date: 2022/8/24 下午 1:39
+     */
+    @Scheduled(cron = "0 * * * * ?")
+    public void noonTask() {
+        WeatherInfo weather = GaodeUtil.getNowWeatherInfo(getAdcCode("湖北省", "武汉市"));
+        logger.info("=====>>>>>使用cron1执行定时任务");
+        logger.info("返回数据{}", JSONUtil.toJsonStr(weather));
+    }
 
-    @Scheduled(fixedDelay = 60000)
-    public void test() {
-//                SendMsg.send();
+    /**
+     * @Description: 每天下午15:30执行一次
+     * @Param: []
+     * @return: void
+     * @Author: cole.zhou
+     * @Date: 2022/8/24 下午 1:39
+     */
+    @Scheduled(cron = "0 * * * * ?")
+    public void afternoonTask() {
+        WeatherInfo weather = GaodeUtil.getNowWeatherInfo(getAdcCode("湖北省", "武汉市"));
+        logger.info("=====>>>>>使用cron1执行定时任务");
+        logger.info("返回数据{}", JSONUtil.toJsonStr(weather));
+    }
+
+    /**
+     * @Description: 每天晚上10:30执行一次
+     * @Param: []
+     * @return: void
+     * @Author: cole.zhou
+     * @Date: 2022/8/24 下午 1:39
+     */
+    @Scheduled(cron = "0 * * * * ?")
+    public void nightTask() {
+        SendMsg.send();
         logger.info("=====>>>>>使用cron2执行定时任务");
     }
-
 }
