f84f3ddf60f80598546f599c516c07f9db46c015
cole.zhou <1253980201@qq.com>
2022-08-29 17:32:07 +0800

    添加土味情话接口

--- src/main/java/com/zjh/everydaymessageserverboot/dingdong/utils/Utils.java
+++ src/main/java/com/zjh/everydaymessageserverboot/dingdong/utils/Utils.java
@@ -1,12 +1,23 @@
 package com.zjh.everydaymessageserverboot.dingdong.utils;
 
 import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONArray;
+import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.zjh.everydaymessageserverboot.dingdong.entity.AncientPoetry;
+import com.zjh.everydaymessageserverboot.dingdong.entity.TianXingRespData;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+
+import static org.apache.logging.log4j.message.MapMessage.MapFormat.JSON;
 
 /**
  *@filename: Utils.java
@@ -15,6 +26,46 @@ import java.util.Date;
  *@Date: 2022-08-24 10:32
  */
 public class Utils {
+
+
+    public static String getCaiHongPi() {
+        //彩虹屁接口 每天100次免费
+        String httpUrl = "http://api.tianapi.com/caihongpi/index?key=b9609d80cfdf734dfb3503f8bda81992";
+        //土味情话接口
+        String qinghua = "http://api.tianapi.com/saylove/index?key=b9609d80cfdf734dfb3503f8bda81992";
+        BufferedReader reader = null;
+        String result = null;
+        StringBuffer sbf = new StringBuffer();
+
+        try {
+            URL url = new URL(qinghua);
+            HttpURLConnection connection = (HttpURLConnection) url
+                    .openConnection();
+            connection.setRequestMethod("GET");
+            InputStream is = connection.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+            String strRead = null;
+            while ((strRead = reader.readLine()) != null) {
+                sbf.append(strRead);
+                sbf.append("\r\n");
+            }
+            reader.close();
+            result = sbf.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        JSONObject jsonObject = JSONUtil.parseObj(result);
+        JSONArray newslist = jsonObject.getJSONArray("newslist");
+        JSONObject content = newslist.getJSONObject(0);
+        String content1 = (String) content.get("content");
+        return content1;
+    }
+
+    public static void main(String[] args) {
+        String caiHongPi = getCaiHongPi();
+        System.out.println("caiHongPi = " + caiHongPi);
+    }
 
 
     /**
